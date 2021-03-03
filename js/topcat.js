d468498418486c8ace32f8747efc21f7cbfc65f3
Sohidul Islam <63615353+Sohidul-Islam@users.noreply.github.com>
2021-03-04 02:37:50 +0600

    Initial commit

---
+++ js/topcat.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    576: {
+      items: 2,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 4,
+    },
+    1140: {
+      items: 4,
+    },
+  },
+});
